509be244256529ac47ff4227e3c97a98f0219f45
mido1 <amarif@tudelft.nl>
2020-03-29 13:21:30 +0200

    added docstring

--- Pair_correlation_plots.py
+++ Pair_correlation_plots.py
@@ -7,6 +7,9 @@ from Source.initialization import *
 from Source.timeEvolution import *
 import Source.variables as vr
 
+""" This file will calculate the pair Correlation function for different densities and temperatures,
+    it will then plot these functions against each other to show the different phases of Argon.
+"""
 
 def calculateCorrelation(density, temp):
 
@@ -27,9 +30,9 @@ def calculateCorrelation(density, temp):
 def corr_plot(gr, bins):
 
     plt.figure()
-    plt.plot(bins[1:201], gr[0], label=fr"$\rho = {density[0]}$, T = {temp[0]}")
-    plt.plot(bins[1:201], gr[1], label=fr"$\rho = {density[1]}$, T = {temp[1]}")
-    plt.plot(bins[1:201], gr[2], label=fr"$\rho = {density[2]}$, T = {temp[2]}")
+    plt.plot(bins[1:201], gr[0],linestyle='-', color = "r", label=fr"$\rho = {density[0]}$, T = {temp[0]}")
+    plt.plot(bins[1:201], gr[1],linestyle='--',color = "c",  label=fr"$\rho = {density[1]}$, T = {temp[1]}")
+    plt.plot(bins[1:201], gr[2], color = "b", label=fr"$\rho = {density[2]}$, T = {temp[2]}")
     plt.xlabel('r')
     plt.ylabel('g(r)')
     plt.title('Pair Correlation Function of Different Phases')
@@ -37,9 +40,8 @@ def corr_plot(gr, bins):
     plt.savefig("plots/pair_correlation_plot.png")
     plt.show()
 
-temp = np.array([1.095, 0.8, 3])
-density = np.array([0.88, 1.5, 0.5])
-
+temp = np.array([1, 0.8, 3])
+density = np.array([1, 1.5, 0.5])
 gr = np.zeros((3,200))
 
 for i in range(len(temp)):
